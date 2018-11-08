5dac6ddc8d4171c93268ceb862d31a76587c3f63
Mitch Gollub <mgollub@live.com>
2018-11-08 07:31:14 -0500

    show sales summary on front end

--- ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -6,18 +6,19 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './fetch-data.component.html'
 })
 export class FetchDataComponent {
-  public forecasts: WeatherForecast[];
+  public summaries: SalesSummary[];
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>(baseUrl + 'api/SalesSummary').subscribe(result => {
-      this.forecasts = result;
+    http.get<SalesSummary[]>(baseUrl + 'api/SalesSummary').subscribe(result => {
+      this.summaries = result;
     }, error => console.error(error));
   }
 }
 
-interface WeatherForecast {
-  dateFormatted: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
+interface SalesSummary {
+  salesRep: string;
+  y2DSold: number;
+  m2DSold: number;
+  q2DSold: number;
+  i2DSold: number;
 }
